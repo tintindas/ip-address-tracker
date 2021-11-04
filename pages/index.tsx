23312b211cd246aa2f063e8f86dd93a6ee164b9d
Upamanyu Das <upamanyu.das.work@gmail.com>
2021-11-04 19:45:05 +0530

    Change initial data

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,35 +6,35 @@ import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
 
 export default function Home() {
+	const GOOGLE_COORDINATES = [37.386051, -122.083855]
+
 	const MapWithNoSSR = dynamic(() => import('../components/Map'), {
 		ssr: false
 	})
 
-	const [result, setResult] = useState({})
-	const [coordinates, setCoordinates] = useState([28.6448, 77.216721])
+	const [result, setResult] = useState({
+		'ip address': '8.8.8.8',
+		location: 'Mountain View, US',
+		timezone: 'UTC -07:00',
+		isp: 'Google LLC'
+	})
+	const [coordinates, setCoordinates] = useState(GOOGLE_COORDINATES)
 
 	useEffect(() => {
-		const fetchIp = async () => {
+		const loadInitialData = async () => {
 			const res = await fetch('https://api.ipify.org/')
 			const ip = await res.text()
+			await search(ip)
 		}
-		fetchIp()
+		loadInitialData()
 	}, [])
 
-	const search = async (ip: FormDataEntryValue) => {
+	const search = async (query: FormDataEntryValue) => {
 		const res = await fetch(
-			`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.NEXT_PUBLIC_IPIFY_APIKEY}&ipAddress=${ip}`
+			`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.NEXT_PUBLIC_IPIFY_APIKEY}&ipAddress=${query}`
 		)
 		const data = await res.json()
-		return data
-	}
-
-	const handleSubmit = async (e) => {
-		e.preventDefault()
-		const data = new FormData(e.target)
-		const { query } = Object.fromEntries(data.entries())
-		const info = await search(query)
-		const { location, isp, ip } = info
+		const { location, isp, ip } = data
 		const result = {
 			'ip address': ip,
 			location: `${location.city}, ${location.country}`,
@@ -43,6 +43,13 @@ export default function Home() {
 		}
 		setCoordinates([location.lat, location.lng])
 		setResult(result)
+	}
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		const data = new FormData(e.target)
+		const { query } = Object.fromEntries(data.entries())
+		await search(query)
 	}
 	return (
 		<div>
